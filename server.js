81bcd5637f8fba83c8618b1209776616a1c3344f
Jahangir Ahamd <jsofi502@gmail.com>
2022-03-11 12:22:54 +0530

    Created Server Using Express.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
