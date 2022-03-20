d27e3ad6f3dd3fce4dc2821e837d42bfca840bd5
Jahangir Ahamd <jsofi502@gmail.com>
2022-03-20 17:54:57 +0530

    Created Server Using Express.

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const dotenv = require('dotenv');
+const axios = require('axios');
+const Posts = require('./models/posts');
+const postRouter = require('./routes/posts');
+const mongoDb = require('./config/db');
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const { PORT } = process.env;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+mongoDb();
+
+app.get('/', async (req, res) => {
+  // fetch all the posts
+  const posts = await Posts.find().sort({ createdAt: 'desc' });
+
+  res.render('index', { posts });
 });
+
+app.use('/', postRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
