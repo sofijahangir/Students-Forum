342f9d59ed392073be2b2fb455763f1fb3a52876
Jahangir Ahamd <jsofi502@gmail.com>
2022-04-07 13:25:13 +0530

    Updated Posts Controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,25 +1,51 @@
 // Post Controller
 
 const Post = require('../models/posts');
+const User = require('../models/user');
+const Comments = require('../models/comments');
 
 // Create Post
 
 const addPost = async (req, res) => {
-  const { userId, title, description, category, image } = req.body;
+  // Fetch userId from session
+  const userId = req.session.passport.user;
+  const author = await User.findById(userId);
+  console.log(author.username);
+  const { title, description, category, image } = req.body;
   const newPost = new Post({
     userId,
+    author: author.username,
     title,
     description,
     category,
     image,
   });
   await newPost.save();
-  res.send(newPost);
+  // console.log(username);
+  res.redirect('/');
 };
-
 const getPosts = async (req, res) => {
   const posts = await Post.find();
   res.send(posts);
 };
 
-module.exports = { addPost, getPosts };
+const readPost = async (req, res) => {
+  // Get the id of the post we want to read that is not in the params
+
+  const postId = req.params.id;
+  const post = await Post.findById(postId);
+
+  // Get all the comment desc of the post
+  const comments = await Comments.find({ postId });
+
+  res.render('posts/readPost', { post, comments });
+};
+
+const getPostsOfUser = async (req, res) => {
+  const userId = req.session.passport.user;
+  const posts = await Post.find({ userId });
+  // console.log(posts);
+  res.render('/dashboard', { posts });
+};
+
+module.exports = { addPost, getPosts, getPostsOfUser, readPost };
