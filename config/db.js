080f69faa9091c17419781d100c3a515cb447c9d
Jahangir Ahamd <jsofi502@gmail.com>
2022-04-07 13:39:22 +0530

    Updated Db Connection

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoConnect = async () => {
-  const connection = await mongoose.connect(process.env.MONGO_URL, {});
+  const connection = await mongoose.connect(process.env.ONLINE_DB, {});
   console.log('Connected to Forum DataBase');
 };
 
