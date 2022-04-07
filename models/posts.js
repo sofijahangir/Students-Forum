50d26ce1a3928352ceb541263e5f4f95096dfda1
Jahangir Ahamd <jsofi502@gmail.com>
2022-04-07 13:24:58 +0530

    Updated Posts Model

--- models/posts.js
+++ models/posts.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    author: {
+      type: String,
+      required: true,
+    },
     title: {
       type: String,
       required: true,
@@ -20,6 +27,13 @@ const postSchema = new mongoose.Schema(
       data: Buffer,
       contentType: String,
     },
+    // Array of comments
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
   },
   { timestamps: true }
 );
