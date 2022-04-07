bd5efb89bcb5e6fba56949255c0df62ca885561c
Jahangir Ahamd <jsofi502@gmail.com>
2022-04-07 13:27:29 +0530

    Updated User Model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const passport = require('passport');
+const passportLocal = require('passport-local');
+
+const PassportLocalSchema = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+UserSchema.virtual('posts', {
+  ref: 'Post',
+  foreignField: 'userId',
+  localField: '_id',
+});
+UserSchema.virtual('comments', {
+  ref: 'Comment',
+  foreignField: 'userId',
+  localField: '_id',
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
