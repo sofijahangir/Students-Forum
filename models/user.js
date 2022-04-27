8e011a097d6d1e3a153701df7e9d19d300d011d4
Jahangir Ahamd <jsofi502@gmail.com>
2022-04-27 14:36:17 +0530

    Updated User Model.

--- models/user.js
+++ models/user.js
@@ -11,6 +11,8 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true },
     phone: { type: Number, required: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    isFaculty: { type: Boolean, default: false },
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
