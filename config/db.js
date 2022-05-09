287ba2a0e195fccd68ef78606c9a33abda4c219e
Jahangir Ahamd <jsofi502@gmail.com>
2022-05-09 11:27:51 +0530

    Updated Conn String.

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const mongoConnect = async () => {
   const connection = await mongoose.connect(
-    'mongodb+srv://jsofi502:forum@cluster0.h4voi.mongodb.net/forum?retryWrites=true&w=majority',
+    'mongodb+srv://jsofi502:forum@cluster0.pd5rt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {}
   );
-  console.log('Connected to Forum DataBase');
+  console.log('Connected to Forum Database.');
 };
 
 module.exports = mongoConnect;
